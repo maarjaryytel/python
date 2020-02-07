cb035c4287b5af96feb7a7551c1cc6849f444c00
esko <eskobrandt@gmx.com>
2020-02-07 19:38:24 +0200

    tund2

---
+++ loop.py
@@ -0,0 +1,46 @@
+#while
+#for
+
+#index=100
+#teineIndex=10
+#print(index)
+
+#while index<=10:
+    
+    #print(teineIndex)
+    #index+=1 
+    #index+=100
+    #if index==5:
+     #   print(index)
+        #break
+        #continue
+#else:
+    #print("Condition is false")
+
+#fruits=["apple", "banana", "cherry"]
+#for x in fruits:
+    #print(x)
+
+#for x in range(2,60,3): #see viimane nr tõstab 3 võrra
+    #print(x)
+    #pass     kui ma ei taha siin midagi kuvada, siis panen pass,
+    #siis ta läheb edasi
+
+#else:
+    #print("finished!")
+
+#for x in range(11): #10 korda
+    #for x in range(11): #10 korda
+        #print(x) #ehk kuvab 2 korda järjest
+
+cars=["Audi", "BMW", "Ford", "Mazda", "Peugeot"]
+print(cars) 
+
+vastus = input("Vali nimekirjast oma lemmik: ")
+
+
+#for car in cars:           see on vale  
+    #print(cars.index(vastus))
+
+for car in cars:
+    print(cars.index(car))                     #(cars.index(vastus))
---
+++ tund2.py
@@ -0,0 +1,24 @@
+#if 3==3 or 4==4:
+    #print("on olemas")
+
+#if 4==4:
+    #if 5==4:
+        #print("on olemas nested if")
+
+##vastus = int(input("Sisesta oma arv: - "))
+
+#if vastus%2==0:
+    #print("Tegemist on paarisarvuga")
+
+#else:
+    #print("Tegemist on paaritu arvuga")
+
+aasta = int(input("Sisesta oma aastaarv: "))
+ 
+if aasta%100==0:
+    if aasta%400!=0:        
+        print("lihtaasta")
+elif aasta%4==0:
+    print("liigaasta")
+else:
+    print("lihtaasta")
